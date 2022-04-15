077e94602ca1dd4ea6838abd16fc8aca3c015675
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-15 12:52:02 -0600

    Made the Server functional with errors and responses

--- api/server.js
+++ api/server.js
@@ -9,9 +9,28 @@ server.use(express.json());
 server.use('/api/actions', actionsRouter); //This could be the wrong end point
 server.use('/api/projects', projectsRouter); //This could be the wrong end point
 
+
+server.get('/', (req, res) => {
+  res.send('This is the working server at /')
+})
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || 'Error retreiving the thingys'
+  })
+})
+
+server.use('*', (req, res) => {
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` })
+})
+
+
+
+
+
+module.exports = server;
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-
-module.exports = server;
